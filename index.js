976148da3b8742cdbe70f99968088d0eaae06817
Sahil <sharma.1901@yahoo.com>
2020-09-16 23:08:29 +0530

    Animations added as per the update

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const chatBot = document.querySelector(".chatbot");
 const chatBotInvoke = document.querySelector(".chatbot-invoke");
 const hand = document.querySelector("#hand");
+const headerTitle = document.querySelector(".chatbot-header__title");
+const subtitleContainer = document.querySelector(".subtitle-container");
+const headerTextOne = document.querySelector(".chatbot-header__text1");
+const headerTextTwo = document.querySelector(".chatbot-header__text2");
+const botIcon = document.querySelector(".chatbot-bot-icon-container");
+const content = document.querySelector(".chatbot-content");
+const faqs = document.querySelectorAll(".faq");
 
 chatBotInvoke.addEventListener("click", () => {
-    chatBot.classList.add("show");
-    chatBotInvoke.classList.add("hidden");
-    hand.classList.add("animated");
+  chatBot.classList.add("show");
+  chatBotInvoke.classList.add("hidden");
+  hand.classList.add("animated");
+  headerTitle.classList.add("animated");
+  subtitleContainer.classList.add("animated");
+  headerTextOne.classList.add("animated");
+  headerTextTwo.classList.add("animated");
+  content.classList.add("animated");
+  faqs.forEach((faq) => faq.classList.add("animated"));
+  botIcon.classList.add("animated");
 });
