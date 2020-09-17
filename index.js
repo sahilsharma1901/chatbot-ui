558c3c63fa3e946159f16c7effff189fb551ccbd
Sahil <sharma.1901@yahoo.com>
2020-09-17 12:43:28 +0530

    Fixed an issue (CTA Button)

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const headerTextTwo = document.querySelector(".chatbot-header__text2");
 const botIcon = document.querySelector(".chatbot-bot-icon-container");
 const content = document.querySelector(".chatbot-content");
 const faqs = document.querySelectorAll(".faq");
+const CTAButton = document.querySelector(".chatbot-CTA-btn");
 
 chatBotInvoke.addEventListener("click", () => {
   chatBot.classList.add("show");
@@ -20,4 +21,19 @@ chatBotInvoke.addEventListener("click", () => {
   content.classList.add("animated");
   faqs.forEach((faq) => faq.classList.add("animated"));
   botIcon.classList.add("animated");
+  CTAButton.classList.add("animated");
+});
+
+chatBot.addEventListener("dblclick", () => {
+  chatBot.classList.remove("show");
+  chatBotInvoke.classList.remove("hidden");
+  hand.classList.remove("animated");
+  headerTitle.classList.remove("animated");
+  subtitleContainer.classList.remove("animated");
+  headerTextOne.classList.remove("animated");
+  headerTextTwo.classList.remove("animated");
+  content.classList.remove("animated");
+  faqs.forEach((faq) => faq.classList.remove("animated"));
+  botIcon.classList.remove("animated");
+  CTAButton.classList.remove("animated");
 });
